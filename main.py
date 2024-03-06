fb3a30c32f110ac3240037bca5ee812c03ec527d
Rohan Garner <161069703+08BitPixels@users.noreply.github.com>
2024-03-06 13:45:19 +0000

    Update main.py, constants.py
    - Continued coding zoom feature

--- main.py
+++ main.py
@@ -15,17 +15,25 @@ clock = pygame.time.Clock()
 
 class Game:
 
-    def __init__(self, world_dimensions: tuple) -> None:
+    def __init__(self, screen_dimensions: tuple, world_dimensions: tuple) -> None:
 
-        self.world = World(world_dimensions)
+        self.SCREEN_DIMENSIONS = screen_dimensions
+        self.zoom = self.SCREEN_DIMENSIONS[0] // 10
+        self.cols, self.rows = self.SCREEN_DIMENSIONS[0] // self.zoom, self.SCREEN_DIMENSIONS[1] // self.zoom
+        self.sq_size = (self.SCREEN_DIMENSIONS[0] / self.cols, self.SCREEN_DIMENSIONS[1] / self.rows)
+        self.grid_width = self.zoom // 20
+        
+        self.world = World(world_dimensions, self)
 
 class World:
 
-    def __init__(self, dimensions: tuple) -> None:
+    def __init__(self, world_dimensions: tuple, game: Game) -> None:
         
         self.ALIVE = 1
         self.DEAD = 0
-        self.dimensions = dimensions
+
+        self.game = game
+        self.dimensions = world_dimensions
         
         self.grid = ndarray((self.dimensions[0], self.dimensions[1]), pygame.sprite.Sprite)
         self.cells = pygame.sprite.Group()
@@ -48,21 +56,26 @@ class World:
 
             for y in range(self.dimensions[1]):
 
-                cell = Cell(pos = (x, y), state = self.DEAD)
+                cell = Cell(pos = (x, y), state = self.DEAD, size = self.game.sq_size, world = self)
                 self.grid[y][x] = cell
                 self.cells.add(cell)
 
     def draw_grid(self) -> None:
 
         # Draws lines for the board
-        for line in range(COLS + 1): # vertical
-            pygame.draw.line(screen, GRID_COLOUR, (SQ_SIZE_X * line, 0), (SQ_SIZE_Y * line, HEIGHT), GRID_WIDTH)
+        for line in range(self.game.cols + 1): # vertical
+            pygame.draw.line(screen, GRID_COLOUR, (self.game.sq_size[0] * line, 0), (self.game.sq_size[1] * line, HEIGHT), self.game.grid_width)
 
-        for line in range(ROWS + 1): # horizontal
-            pygame.draw.line(screen, GRID_COLOUR, (0, SQ_SIZE_X * line), (WIDTH, SQ_SIZE_Y * line), GRID_WIDTH)
+        for line in range(self.game.rows + 1): # horizontal
+            pygame.draw.line(screen, GRID_COLOUR, (0, self.game.sq_size[0] * line), (WIDTH, self.game.sq_size[1] * line), self.game.grid_width)
 
         pygame.display.update() # Update the screen
 
+    def calc_grid(self, sq_size: tuple) -> None:
+
+        self.sq_size = sq_size
+        for cell in self.cells.sprites(): cell.calc_size(self.sq_size)
+    
     def toggle_cell(self, coords: tuple) -> None:
 
         x, y = coords
@@ -72,15 +85,17 @@ class World:
 
 class Cell(pygame.sprite.Sprite):
 
-    def __init__(self, pos: tuple, state: int) -> None:
+    def __init__(self, pos: tuple, size: tuple, state: int, world: World) -> None:
 
         super().__init__()
 
-        self.image = pygame.Surface((SQ_SIZE_X, SQ_SIZE_Y))
-        self.rect = self.image.get_rect(topleft = (pos[0] * SQ_SIZE_X, pos[1] * SQ_SIZE_Y))
+        self.world = world
+        self.state = state
+        self.size = size
         self.grid_pos = pygame.math.Vector2(pos)
 
-        self.state = state
+        self.image = pygame.Surface(self.size)
+        self.rect = self.image.get_rect(topleft = (self.grid_pos[0] * self.size[0], self.grid_pos[1] * self.size[1]))
 
     def update(self) -> None:
 
@@ -95,11 +110,14 @@ class Cell(pygame.sprite.Sprite):
     def update_colour(self) -> None:
         self.image.fill([CELL_DEAD, CELL_ALIVE][self.state])
 
+    def calc_size(self, size: tuple) -> None:
+
+        self.image = pygame.transform.scale(self.image, size)
+        self.rect = self.image.get_rect(topleft = (self.grid_pos[0] * self.world.game.sq_size[0], self.grid_pos[1] * self.world.game.sq_size[0]))
+
 def main():
 
-    global zoom
-
-    game = Game((WORLD_X, WORLD_Y))
+    game = Game(screen_dimensions = (WIDTH, HEIGHT), world_dimensions = (WORLD_X, WORLD_Y))
     world = game.world
 
     while True:
@@ -113,14 +131,19 @@ def main():
 
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
 
-                x = int(event.pos[0] // SQ_SIZE_X)
-                y = int(event.pos[1] // SQ_SIZE_Y)
+                x = int(event.pos[0] // game.sq_size[0])
+                y = int(event.pos[1] // game.sq_size[1])
 
                 if x <= WORLD_X and y <= WORLD_Y: world.toggle_cell((x, y))
 
             if event.type == pygame.MOUSEWHEEL:
 
-                zoom *= event.y * SCROLL_SPEED
+                game.zoom += event.y * SCROLL_SPEED
+                game.cols, game.rows = WIDTH // game.zoom, HEIGHT // game.zoom
+                game.sq_size = (WIDTH / game.cols, HEIGHT / game.rows)
+                game.grid_width = game.zoom // 20
+
+                world.calc_grid(game.sq_size)
 
         screen.fill('White')
 
